b2f1d76ccc4df7fdad6fac72d7f123e56c9b6277
kelly <kellyworld3@gmail.com>
2021-10-10 12:23:33 +0800

    update hw1

---
+++ hw2/main.js
@@ -0,0 +1,50 @@
+function changeImage(imgs) {
+    var expandImg = document.getElementById("bigPhoto");
+    expandImg.src = imgs.src;
+}
+
+function dog(button){
+    let showImg1=document.getElementById("smallPhoto1");
+    let showImg2=document.getElementById("smallPhoto2");
+    let showImg3=document.getElementById("smallPhoto3");
+    let showImg4=document.getElementById("smallPhoto4");
+    let expandImg = document.getElementById("bigPhoto");
+    showImg1.src="https://www.science.org/do/10.1126/science.aba2340/abs/dogs_1280p_0.jpg";
+    showImg2.src="https://chico.ca.us/sites/main/files/imagecache/lightbox/main-images/dog_license.jpg";
+    showImg3.src="https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/HB4AT3D3IMI6TMPTWIZ74WAR54.jpg";
+    showImg4.src="https://media.nature.com/lw800/magazine-assets/d41586-020-01430-5/d41586-020-01430-5_17977552.jpg";
+    expandImg.src="https://media.nature.com/lw800/magazine-assets/d41586-020-01430-5/d41586-020-01430-5_17977552.jpg";
+}
+
+function cat(button){
+    // cat.classList.add("choose");
+    let showImg1=document.getElementById("smallPhoto1");
+    let showImg2=document.getElementById("smallPhoto2");
+    let showImg3=document.getElementById("smallPhoto3");
+    let showImg4=document.getElementById("smallPhoto4");
+    let expandImg = document.getElementById("bigPhoto");
+    showImg1.src="https://med.stanford.edu/news/all-news/2021/09/cat-fur-color-patterns/_jcr_content/main/image.img.620.high.jpg/cat_by-Kateryna-T-Unsplash.jpg";
+    showImg2.src="https://c0.wallpaperflare.com/preview/793/890/349/tabby-cat-looking-upward.jpg";
+    showImg3.src="https://www.thesprucepets.com/thmb/5YOHbja8lLg4KprPlJsNvS9mq50=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/guide-to-cat-eyes-552114-hero-75d820458de24543a35543a584b9eec6.jpg";
+    showImg4.src="https://mymodernmet.com/wp/wp-content/uploads/archive/J0R3qpz33gRJaWzcZ3eB_1082105479.jpeg";
+    expandImg.src="https://med.stanford.edu/news/all-news/2021/09/cat-fur-color-patterns/_jcr_content/main/image.img.620.high.jpg/cat_by-Kateryna-T-Unsplash.jpg";
+    
+}
+
+function rabbit(button){
+    let showImg1=document.getElementById("smallPhoto1");
+    let showImg2=document.getElementById("smallPhoto2");
+    let showImg3=document.getElementById("smallPhoto3");
+    let showImg4=document.getElementById("smallPhoto4");
+    let expandImg = document.getElementById("bigPhoto");
+    showImg1.src="https://www.genengnews.com/wp-content/uploads/2021/03/aswathy-n-srMHHWCTvcU-unsplash-1068x801.jpg";
+    showImg2.src="https://www.massaudubon.org/var/ezdemo_site/storage/images/site_ma/learn/nature-wildlife/mammals/cottontail-rabbits/situations-solutions/176132-10-eng-US/situations-solutions.jpg";
+    showImg3.src="https://s.abcnews.com/images/Business/GTY_rabbit_sr_140508_16x9_1600.jpg";
+    showImg4.src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1414,w_2102,x_0,y_0/v1554351612/shape/mentalfloss/557234-istock-480927021.jpg?itok=PqZrVPfq";
+    expandImg.src="https://www.genengnews.com/wp-content/uploads/2021/03/aswathy-n-srMHHWCTvcU-unsplash-1068x801.jpg";
+}
+
+
+
+
+
